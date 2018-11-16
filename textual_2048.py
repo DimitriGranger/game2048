538ecdf7277e85f173f6c7a42477db6fdcf3ed51
DimitriGranger <45032273+DimitriGranger@users.noreply.github.com>
2018-11-16 11:07:18 +0100

    Update textual_2048.py

--- textual_2048.py
+++ textual_2048.py
@@ -2,6 +2,7 @@ from game2048.grid_2048 import *
 
 
 def read_player_command():
+    """Demande à l'utilisateur de donner une commande et la renvoie."""
     move = 0
     while not move in ['g','d','h','b'] :
         move = input("Entrez votre commande (g (gauche), d (droite), h (haut), b (bas)):")
@@ -10,12 +11,14 @@ def read_player_command():
 
 
 def read_size_grid():
+    """Demande à l'utilisateur de donner la taille de grille qu'il veut et la renvoie."""
     size = input("Entrez la taille de grille voulue.")
 
     return size
 
 
 def read_theme_grid():
+    """Demande à l'utilisateur le thème qu'il veut et le renvoie."""
     theme = input("Entrez le thème voulu.")
 
     while not theme in ['0','1','2']:
